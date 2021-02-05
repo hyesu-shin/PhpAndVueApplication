a1d35c3aea57b9ce79b3aa2a236b9d5287a7a578
hyesu <peche3@naver.com>
2021-02-05 20:18:28 +0900

    Fix Config.php to access heroku mysql

--- public/core/Config.php
+++ public/core/Config.php
@@ -3,10 +3,10 @@
 
   return [
     'database' => [
-      'name' => 'backend',
-      'username' => 'hyesu',
-      'password' => 'hyesu',
-      'connection' => 'mysql:host=localhost',
+      'name' => 'heroku_552505a6705c415',
+      'username' => 'bbbc30981e2c16',
+      'password' => '28edd2d9',
+      'connection' => 'mysql:host=us-cdbr-east-03.cleardb.com',
       'options' => [
         // 에러 발생시 어떠한 에러가 발생하는지 알기 위한 옵션
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
