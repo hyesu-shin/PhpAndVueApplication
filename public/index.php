9be7673a5b0f152a2823d692dd9c694b47f27219
hyesu <peche3@naver.com>
2021-02-08 18:18:37 +0900

    Fix index.php file to connect vue front

--- public/index.php
+++ public/index.php
@@ -28,16 +28,16 @@
   //   }
   // }
   
-  // // Bootstrap.php를 호출함으로써, router, request클래스 등 router에 필요한 모든 로직 불러옴
-  // require 'core/Bootstrap.php';
+  // Bootstrap.php를 호출함으로써, router, request클래스 등 router에 필요한 모든 로직 불러옴
+  require 'core/Bootstrap.php';
   
-  // // uri list인 routes 불러오기
-  // $router = Router::load('Routes.php'); 
+  // uri list인 routes 불러오기
+  $router = Router::load('Routes.php'); 
 
-  // // request_uri가 어떻게 들어오는지 알아야 함
-  // $uri = Request::uri();
+  // request_uri가 어떻게 들어오는지 알아야 함
+  $uri = Request::uri();
 
-  // // Router클래스의 메서드 : routes존재 여부 검사 후 redirect
-  // require $router->direct($uri); 
-  require $_SERVER["DOCUMENT_ROOT"].'/dist/index.html';
+  // Router클래스의 메서드 : routes존재 여부 검사 후 redirect
+  require $router->direct($uri); 
+
 ?>
