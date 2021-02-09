87960754e2c9083d9f38da893d3574be5f189902
hyesu <peche3@naver.com>
2021-02-09 13:53:56 +0900

    Fix index.php file to connect vue front

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
   $uri = Request::uri();
   $_uri = explode('/', $uri);    // uri를 ('/') 단위로 쪼갬
 
-  if($_url[0] == 'api') {
+  if($_url[0] == "api") {
     // Router클래스의 메서드 : routes존재 여부 검사 후 redirect
     require $router->direct($uri); 
   }
